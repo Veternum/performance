64cb956c97c9553b1e18564653a4210e58e9951b
Veternum <hans.rhesa88@gmail.com>
2021-03-06 11:31:39 +0700

    Try

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IData {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface IAppState {
+  showTable: boolean; 
+}
+
+interface IAppProps {
+  data: IData[]
+}
+
+class App extends React.Component<IAppProps, IAppState> {
+  public toggleShowTable = () => {
+    const { showTable } = this.state;
+    this.setState({showTable: !showTable});
+  }
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+              <button onClick={this.toggleShowTable}>test</button>
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
